af19a87e0f299df6b131035121b416db3eab4ed5
Max Kraan <17745834+mxkrn@users.noreply.github.com>
2021-08-06 17:02:54 +0200

    Prettier formatting

--- js/audio-context.js
+++ js/audio-context.js
@@ -1,6 +1,6 @@
-document.querySelector('body').addEventListener('click', async () => {
-	await Tone.start();
-	console.log("context started");
+document.querySelector("body").addEventListener("click", async () => {
+  await Tone.start();
+  console.log("context started");
 });
 
 export default {};
--- js/players.js
+++ js/players.js
@@ -4,17 +4,21 @@ const reverb = new Tone.Reverb(0.2);
 const crush = new Tone.BitCrusher(9);
 
 export const sampleMap = {
-  'Kick Drum': '../samples/808/kick.wav',
-  'Snare Drum': '../samples/808/snare.wav',
-  'Hi-Hat Closed': '../samples/808/hh.wav',
-  'Hi-Hat Open': '../samples/808/oh.wav',
-  'High Tom': '../samples/808/ht.wav', 
-  'High-Mid Tom': '../samples/808/mt.wav',
-  'Low Tom': '../samples/808/lt.wav',
-  'Crash Cymbal': '../samples/808/cym.wav',
-  'Ride Cymbal': '../samples/808/rim.wav',
-}; 
+  "Kick Drum": "../samples/808/kick.wav",
+  "Snare Drum": "../samples/808/snare.wav",
+  "Hi-Hat Closed": "../samples/808/hh.wav",
+  "Hi-Hat Open": "../samples/808/oh.wav",
+  "High Tom": "../samples/808/ht.wav",
+  "High-Mid Tom": "../samples/808/mt.wav",
+  "Low Tom": "../samples/808/lt.wav",
+  "Crash Cymbal": "../samples/808/cym.wav",
+  "Ride Cymbal": "../samples/808/rim.wav",
+};
 
-export const players = new Tone.Players (sampleMap).chain(eq, crush, gain, reverb, Tone.Destination);
-
-// export const getAudioSamplesLoaded = players.loaded;
+export const players = new Tone.Players(sampleMap).chain(
+  eq,
+  crush,
+  gain,
+  reverb,
+  Tone.Destination
+);
